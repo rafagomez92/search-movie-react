a17322ba76486fce0a0738cae26c355e8764d699
rafagomez92 <rafael_gomez92@hotmail.com>
2020-04-04 02:07:18 -0600

    In this part is the pages of the app

---
+++ src/pages/Detail.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ButtonBackToHome } from '../components/ButtonBackToHome';
+
+const API_KEY = '4287ad07'
+
+export class Detail extends Component {
+    static propTypes = {
+        match: PropTypes.shape({
+            params: PropTypes.object,
+            isExact: PropTypes.bool,
+            path: PropTypes.string,
+            url: PropTypes.string
+        })
+    }
+
+    state = { movie: {} }
+
+    _fetchMovies ({ id }) {
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+          .then(res => res.json())
+          .then(movie => {        
+            console.log({ movie });                        
+            this.setState({ movie })
+          })
+    }
+
+    _goBack() {
+        window.history.back();
+    }
+    componentDidMount () {
+        const { movieId } = this.props.match.params;
+        this._fetchMovies({ id: movieId });
+    }
+    render() {
+        const { Title, Poster, Actors, Metascore, Plot } = this.state.movie
+        return (
+            <div>
+                <ButtonBackToHome />
+                <h1>{Title}</h1>
+                <img src={Poster} alt='Movie poster'/>
+                <h3>{Actors}</h3>
+                <span>{Metascore}</span>                
+                <p>{Plot}</p>
+            </div>
+        )
+    }
+}
